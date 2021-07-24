b944249663e34bd0f2d2d25f64c8a60ab0600da8
SH-USERNAME <75603273+SH-USERNAME@users.noreply.github.com>
2021-07-24 16:35:43 +0630

    Add files via upload

---
+++ Prime.sh
@@ -0,0 +1,766 @@
+#!bin/bash
+clear
+apt install sl -y
+apt install toilet -y
+apt install cowsay -y
+clear
+echo -e "\e[1;34m"
+toilet -f ivrit _P R I M E ! _
+echo -e "\e[1;34m
+ ###################################################
+ ##                Myanmar User                   ##
+ ##-----------------------------------------------##
+ ##  We are not Hacker,We are helper              ##
+ ##  Actor Team ::: PRIME/username and password   ##
+ ##          Will be show only our page           ##
+ ##  note: This Tool is only for begginer.        ##
+ ###################################################
+ "
+date && cal
+echo "
+ ---------------------------------------------------
+ "
+user=prime
+password=myanmar
+
+read -p $'\e[2;39;31mEnter Tool username .:.$' name
+echo "
+ ---------------------------------------------------
+ "
+read -sp $'Enter Tool Password  .:.$ ' Pass
+
+if [[ $name == $user && $pass == $Password ]];then
+echo ""
+echo "Wellcome my tool"
+echo "Please wait..."
+sleep 0.3
+clear
+echo "pLease wait.."
+sleep 0.3
+clear 
+echo "plEase wait. "
+sleep 0.3
+clear
+echo "pleAse wait..."
+sleep 0.3
+clear
+echo "pleaSe wait.."
+sleep 0.3
+clear
+echo "pleasE wait. "
+sleep 0.3
+clear
+echo "please Wait..."
+sleep 0.3
+clear
+echo "please wAit.."
+sleep 0.3
+clear
+echo "please waIt. "
+sleep 0.3
+clear
+echo "please waiT.."
+sleep 0.3
+clear
+
+echo "please wait..."
+sleep 0.3
+clear
+echo ""
+sleep 0.3
+clear
+echo "please wait.."
+sleep 0.3
+clear
+echo ""
+sleep 0.3
+clear
+
+echo "package installation is finish!"
+sleep 2
+
+clear
+sl --help
+clear
+
+ sleep 1
+ echo "MYANMAR"
+ sleep 1
+ clear
+ echo ""
+ sleep 1
+ clear
+echo "MYANMAR"
+ sleep 1
+ clear
+ echo ""
+ sleep 1
+ clear
+echo "MYANMAR"
+ sleep 1
+ clear
+ echo ""
+ sleep 1
+ clear
+echo "MYANMAR"
+ sleep 1
+ clear
+ echo ""
+ sleep 1
+ clear
+toilet -f ivrit _MYANMAR_
+cowsay -f kiss ​Sor Shi Lar Bro ?
+
+echo "
+ ---------------------------------------------------
+ "
+ echo -e "\e[1;34m
+ => 1. What is Termux?
+ => 2. Download Termux guide Apk (myanmar language       )credit to owner. 
+ => 3. Tool-X (more than 350 Tools)
+ => 4. Metasploit installation
+ => 5. nmap (port-scanning)
+ => 6. SQLmap (SQL injection)
+ => 00.exit"
+echo "
+ ---------------------------------------------------
+ "
+ 
+ read -p "Enter number => " x
+ 
+echo "
+ ---------------------------------------------------
+ " 
+ case $x in 
+ 1)
+ echo -e "\e[4;37;41mWhat is Termux? \e[0m"
+ echo -e "\e[1;37;36m
+        Termux is an Android terminal emulator and    Linux environment app that works directly with no rooting or setup required. A minimal base system is  installed automatically - additional packages are available using the APT package manager.
+  https://termux.com
+Termux  Developer: Fredrik Fornwall\e[0m
+ ";;
+ 
+ 
+ 2)
+ xdg-open https://play.google.com/store/apps/details?id=com.trthtermuxguide.termux ;;
+ 
+ 3)
+echo "
+Tool-X
+
+Tool-X is considered the best Termux hacking tool since it was specially created for Termux. With Tool-X’s help, one can download more than 370 hacking tools for Termux with just a click.
+
+Rajkumar Dusad created the Tool-X. Before Dusad invented this tool, one had to install all the required Termux hacking tools by going on Github.
+
+This tool has a list of all necessary Termux hacking tools. Post-installation, Tool-X in Termux can be easily installed in your hacking tools with one-click.
+ ---------------------------------------------------
+Command to install Tool-X on Termux
+ ---------------------------------------------------
+pkg update && pkg upgrade -y
+
+pkg install git
+
+git clone https://github.com/Rajkumrdusad/Tool-X.git
+
+cd Tool-X
+
+chmod +x install.aex
+
+sh install. aex
+
+./install.aex
+
+Tool-X
+
+ 
+
+";;
+
+
+ 4)
+ echo "
+Metasploit
+
+Metasploit is not just a Termux hacking tool. It is an entire framework of exploit tools. Metasploit is an infrastructure that one can use to make a set of hacking tools.
+
+Metasploit is a freemium tool, but it is one of the most popular instruments in cybersecurity. It allows white-hat hackers to find vulnerabilities on various platforms.
+
+Metasploit has a community of more than 200,000 users and employees to solve queries and information needed. It also helps to identify any vulnerabilities in the system.
+
+Metasploit is one of the best Termux tools for android hacking.
+ "
+echo "
+ ---------------------------------------------------
+ "
+ sleep 1
+ echo "
+ => 1. Commands to install Metasploit on Termux
+ => 2. To install Metasploit
+ "
+echo "
+ ---------------------------------------------------
+ "
+ read -p "Enter number =>" y
+echo "
+ ---------------------------------------------------
+ ";;
+ 
+5)
+  echo "
+Nmap
+
+Nmap, aka Network Mapper, is a port-scanning software. This is a free hacking tool, and it is viral amongst port scanners for fast network discovery, alerting, and security monitoring.
+
+Nmap uses a wide range of services, and it utilizes raw IP packets to check if the hosts are available in the network. Nmap also lists down the services with critical information, OSs, types of firewalls, and other information.
+
+Nmap has console support as well as GUI application support. But in Termux, one should only use the CLI of Nmap.
+  "
+echo "
+ ---------------------------------------------------
+ "
+ sleep 1
+ echo "
+ => 1. Commands to install Nmap on Termux
+ => 2. To install Nmap
+ "
+echo "
+ ---------------------------------------------------
+ "
+ read -p "Enter number =>" n
+echo "
+ ---------------------------------------------------
+ ";;
+
+6)
+echo "
+SQLmap
+
+The SQLMAP tool allows you to inspect websites for the existence of any SQL injection vulnerabilities. Such as XSS vulnerabilities, or exploit SQL injection.
+
+SQLmap supports various types of SQL injections and a variety of databases.
+
+With the help of SQLmap, it is possible to run checks on the website to inspect if any website is vulnerable to SQL injection or not.
+
+SQLmap is accessible for all the major platforms. One can easily add SQLMAP on Termux.
+ "
+echo "
+ ---------------------------------------------------
+ "
+ sleep 1
+ echo "
+Commands to install SQLmap on Termux
+ ---------------------------------------------------
+ 
+pkg update && pkg upgrade -y
+
+apt install python python2
+
+pkg install git
+
+git clone https://github.com/sqlmapproject/sqlmap
+
+cd sqlmap
+
+chmod +x sqlmap.py
+
+python2 sqlmap.py
+
+ ---------------------------------------------------
+ ";;
+
+ 00)
+ echo "bye"
+ exit;;
+
+ *)
+ echo "Sorry,bye";;
+
+
+esac
+ case $y in
+ 1)
+ echo "
+pkg update && pkg upgrade -y
+pkg install unstable-repo 
+pkg install metasploit 
+
+Usage: mfsconsole
+";;
+2)
+pkg update && pkg upgrade -y
+pkg install unstable-repo -y
+pkg install metasploit -y
+echo "
+ ---------------------------------------------------
+ "
+echo "metasploit installation is finished, Usage:
+mfsconsole 
+  ";;
+  *)
+  ;;
+
+esac
+
+case $n in
+1)
+echo "
+pkg update && pkg upgrade -y
+
+pkg install curl
+
+pkg install Nmap
+
+Usage: nmap www.example.com
+";;
+
+2)
+pkg update && pkg upgrade -y
+
+pkg install curl -y
+
+pkg install Nmap -y
+echo "
+(nmap installation is finished!)";;
+
+*)
+;;
+
+esac
+
+echo "
+ ---------------------------------------------------
+ "
+ echo -e "\e[1;34m
+ => 1. What is Termux?
+ => 2. Download Termux guide Apk (myanmar language       )credit to owner. 
+ => 3. Tool-X (more than 350 Tools)
+ => 4. Metasploit installation
+ => 5. nmap (port-scanning)
+ => 6. SQLmap (SQL injection)
+ => 00.exit"
+echo "
+ ---------------------------------------------------
+ "
+ 
+ read -p "Enter number => " x
+ 
+echo "
+ ---------------------------------------------------
+ " 
+ case $x in 
+ 1)
+ echo -e "\e[4;37;41mWhat is Termux? \e[0m"
+ echo -e "\e[1;37;36m
+        Termux is an Android terminal emulator and    Linux environment app that works directly with no rooting or setup required. A minimal base system is  installed automatically - additional packages are available using the APT package manager.
+  https://termux.com
+Termux  Developer: Fredrik Fornwall\e[0m
+ ";;
+ 
+ 
+ 2)
+ xdg-open https://play.google.com/store/apps/details?id=com.trthtermuxguide.termux ;;
+ 
+ 3)
+echo "
+Tool-X
+
+Tool-X is considered the best Termux hacking tool since it was specially created for Termux. With Tool-X’s help, one can download more than 370 hacking tools for Termux with just a click.
+
+Rajkumar Dusad created the Tool-X. Before Dusad invented this tool, one had to install all the required Termux hacking tools by going on Github.
+
+This tool has a list of all necessary Termux hacking tools. Post-installation, Tool-X in Termux can be easily installed in your hacking tools with one-click.
+ ---------------------------------------------------
+Command to install Tool-X on Termux
+ ---------------------------------------------------
+pkg update && pkg upgrade -y
+
+pkg install git
+
+git clone https://github.com/Rajkumrdusad/Tool-X.git
+
+cd Tool-X
+
+chmod +x install.aex
+
+sh install. aex
+
+./install.aex
+
+Tool-X
+
+ 
+
+";;
+
+
+ 4)
+ echo "
+Metasploit
+
+Metasploit is not just a Termux hacking tool. It is an entire framework of exploit tools. Metasploit is an infrastructure that one can use to make a set of hacking tools.
+
+Metasploit is a freemium tool, but it is one of the most popular instruments in cybersecurity. It allows white-hat hackers to find vulnerabilities on various platforms.
+
+Metasploit has a community of more than 200,000 users and employees to solve queries and information needed. It also helps to identify any vulnerabilities in the system.
+
+Metasploit is one of the best Termux tools for android hacking.
+ "
+echo "
+ ---------------------------------------------------
+ "
+ sleep 1
+ echo "
+ => 1. Commands to install Metasploit on Termux
+ => 2. To install Metasploit
+ "
+echo "
+ ---------------------------------------------------
+ "
+ read -p "Enter number =>" y
+echo "
+ ---------------------------------------------------
+ ";;
+ 
+5)
+  echo "
+Nmap
+
+Nmap, aka Network Mapper, is a port-scanning software. This is a free hacking tool, and it is viral amongst port scanners for fast network discovery, alerting, and security monitoring.
+
+Nmap uses a wide range of services, and it utilizes raw IP packets to check if the hosts are available in the network. Nmap also lists down the services with critical information, OSs, types of firewalls, and other information.
+
+Nmap has console support as well as GUI application support. But in Termux, one should only use the CLI of Nmap.
+  "
+echo "
+ ---------------------------------------------------
+ "
+ sleep 1
+ echo "
+ => 1. Commands to install Nmap on Termux
+ => 2. To install Nmap
+ "
+echo "
+ ---------------------------------------------------
+ "
+ read -p "Enter number =>" n
+echo "
+ ---------------------------------------------------
+ ";;
+
+6)
+echo "
+SQLmap
+
+The SQLMAP tool allows you to inspect websites for the existence of any SQL injection vulnerabilities. Such as XSS vulnerabilities, or exploit SQL injection.
+
+SQLmap supports various types of SQL injections and a variety of databases.
+
+With the help of SQLmap, it is possible to run checks on the website to inspect if any website is vulnerable to SQL injection or not.
+
+SQLmap is accessible for all the major platforms. One can easily add SQLMAP on Termux.
+ "
+echo "
+ ---------------------------------------------------
+ "
+ sleep 1
+ echo "
+Commands to install SQLmap on Termux
+ ---------------------------------------------------
+ 
+pkg update && pkg upgrade -y
+
+apt install python python2
+
+pkg install git
+
+git clone https://github.com/sqlmapproject/sqlmap
+
+cd sqlmap
+
+chmod +x sqlmap.py
+
+python2 sqlmap.py
+
+ ---------------------------------------------------
+ ";;
+
+ 00)
+ echo "bye"
+ exit;;
+
+ *)
+ echo "Sorry,bye";;
+
+
+esac
+ case $y in
+ 1)
+ echo "
+pkg update && pkg upgrade -y
+pkg install unstable-repo 
+pkg install metasploit 
+
+Usage: mfsconsole
+";;
+2)
+pkg update && pkg upgrade -y
+pkg install unstable-repo -y
+pkg install metasploit -y
+echo "
+ ---------------------------------------------------
+ "
+echo "metasploit installation is finished, Usage:
+mfsconsole 
+  ";;
+  *)
+  ;;
+
+esac
+
+case $n in
+1)
+echo "
+pkg update && pkg upgrade -y
+
+pkg install curl
+
+pkg install Nmap
+
+Usage: nmap www.example.com
+";;
+
+2)
+pkg update && pkg upgrade -y
+
+pkg install curl -y
+
+pkg install Nmap -y
+echo "
+(nmap installation is finished!)";;
+
+*)
+;;
+
+esac
+
+echo "
+ ---------------------------------------------------
+ "
+ echo -e "\e[1;34m
+ => 1. What is Termux?
+ => 2. Download Termux guide Apk (myanmar language       )credit to owner. 
+ => 3. Tool-X (more than 350 Tools)
+ => 4. Metasploit installation
+ => 5. nmap (port-scanning)
+ => 6. SQLmap (SQL injection)
+ => 00.exit"
+echo "
+ ---------------------------------------------------
+ "
+ 
+ read -p "Enter number => " x
+ 
+echo "
+ ---------------------------------------------------
+ " 
+ case $x in 
+ 1)
+ echo -e "\e[4;37;41mWhat is Termux? \e[0m"
+ echo -e "\e[1;37;36m
+        Termux is an Android terminal emulator and    Linux environment app that works directly with no rooting or setup required. A minimal base system is  installed automatically - additional packages are available using the APT package manager.
+  https://termux.com
+Termux  Developer: Fredrik Fornwall\e[0m
+ ";;
+ 
+ 
+ 2)
+ xdg-open https://play.google.com/store/apps/details?id=com.trthtermuxguide.termux ;;
+ 
+ 3)
+echo "
+Tool-X
+
+Tool-X is considered the best Termux hacking tool since it was specially created for Termux. With Tool-X’s help, one can download more than 370 hacking tools for Termux with just a click.
+
+Rajkumar Dusad created the Tool-X. Before Dusad invented this tool, one had to install all the required Termux hacking tools by going on Github.
+
+This tool has a list of all necessary Termux hacking tools. Post-installation, Tool-X in Termux can be easily installed in your hacking tools with one-click.
+ ---------------------------------------------------
+Command to install Tool-X on Termux
+ ---------------------------------------------------
+pkg update && pkg upgrade -y
+
+pkg install git
+
+git clone https://github.com/Rajkumrdusad/Tool-X.git
+
+cd Tool-X
+
+chmod +x install.aex
+
+sh install. aex
+
+./install.aex
+
+Tool-X
+
+ 
+
+";;
+
+
+ 4)
+ echo "
+Metasploit
+
+Metasploit is not just a Termux hacking tool. It is an entire framework of exploit tools. Metasploit is an infrastructure that one can use to make a set of hacking tools.
+
+Metasploit is a freemium tool, but it is one of the most popular instruments in cybersecurity. It allows white-hat hackers to find vulnerabilities on various platforms.
+
+Metasploit has a community of more than 200,000 users and employees to solve queries and information needed. It also helps to identify any vulnerabilities in the system.
+
+Metasploit is one of the best Termux tools for android hacking.
+ "
+echo "
+ ---------------------------------------------------
+ "
+ sleep 1
+ echo "
+ => 1. Commands to install Metasploit on Termux
+ => 2. To install Metasploit
+ "
+echo "
+ ---------------------------------------------------
+ "
+ read -p "Enter number =>" y
+echo "
+ ---------------------------------------------------
+ ";;
+ 
+5)
+  echo "
+Nmap
+
+Nmap, aka Network Mapper, is a port-scanning software. This is a free hacking tool, and it is viral amongst port scanners for fast network discovery, alerting, and security monitoring.
+
+Nmap uses a wide range of services, and it utilizes raw IP packets to check if the hosts are available in the network. Nmap also lists down the services with critical information, OSs, types of firewalls, and other information.
+
+Nmap has console support as well as GUI application support. But in Termux, one should only use the CLI of Nmap.
+  "
+echo "
+ ---------------------------------------------------
+ "
+ sleep 1
+ echo "
+ => 1. Commands to install Nmap on Termux
+ => 2. To install Nmap
+ "
+echo "
+ ---------------------------------------------------
+ "
+ read -p "Enter number =>" n
+echo "
+ ---------------------------------------------------
+ ";;
+
+6)
+echo "
+SQLmap
+
+The SQLMAP tool allows you to inspect websites for the existence of any SQL injection vulnerabilities. Such as XSS vulnerabilities, or exploit SQL injection.
+
+SQLmap supports various types of SQL injections and a variety of databases.
+
+With the help of SQLmap, it is possible to run checks on the website to inspect if any website is vulnerable to SQL injection or not.
+
+SQLmap is accessible for all the major platforms. One can easily add SQLMAP on Termux.
+ "
+echo "
+ ---------------------------------------------------
+ "
+ sleep 1
+ echo "
+Commands to install SQLmap on Termux
+ ---------------------------------------------------
+ 
+pkg update && pkg upgrade -y
+
+apt install python python2
+
+pkg install git
+
+git clone https://github.com/sqlmapproject/sqlmap
+
+cd sqlmap
+
+chmod +x sqlmap.py
+
+python2 sqlmap.py
+
+ ---------------------------------------------------
+ ";;
+
+ 00)
+ echo "bye"
+ exit;;
+
+ *)
+ echo "Sorry,bye";;
+
+
+esac
+ case $y in
+ 1)
+ echo "
+pkg update && pkg upgrade -y
+pkg install unstable-repo 
+pkg install metasploit 
+
+Usage: mfsconsole
+";;
+2)
+pkg update && pkg upgrade -y
+pkg install unstable-repo -y
+pkg install metasploit -y
+echo "
+ ---------------------------------------------------
+ "
+echo "metasploit installation is finished, Usage:
+mfsconsole 
+  ";;
+  *)
+  ;;
+
+esac
+
+case $n in
+1)
+echo "
+pkg update && pkg upgrade -y
+
+pkg install curl
+
+pkg install Nmap
+
+Usage: nmap www.example.com
+";;
+
+2)
+pkg update && pkg upgrade -y
+
+pkg install curl -y
+
+pkg install Nmap -y
+echo "
+(nmap installation is finished!)";;
+
+*)
+;;
+
+esac
+else
+echo ""
+echo "Sorry,Incorrect username and Password"
+echo -e "\e[1;34mIf you don't know username and password,follow our page:\e[0m"
+xdg-open https://www.facebook.com/prime.127.0.0.1.007/
+fi
